0eb897fe6ad80d92f4abc6fa53950ecea89027f1
ak9memovies <132725786+ak9memovies@users.noreply.github.com>
2023-11-06 14:55:36 -0600

    Update EncodedURL.js

--- public/js/EncodedURL.js
+++ public/js/EncodedURL.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const proxyForm = document.getElementById('proxyForm');
-  const urlInput = document.getElementById('urlInput');
-  const proxyButton = document.getElementById('proxyButton');
-  
+    const proxyForm = document.getElementById('proxyForm');
+    const urlInput = document.getElementById('urlInput');
 
-  proxyButton.addEventListener('click', function() {
-    const targetURL = urlInput.value;
-    window.location.href = '/mathcalculator?url=' + encodeURIComponent(targetURL);
+    proxyForm.addEventListener('submit', async function(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        const targetURL = urlInput.value;
+
+        // Fetch the proxied content, including the headers
+        const response = await fetch(`/mathcalculator?url=${encodeURIComponent(targetURL)}`);
+        const contentTitle = response.headers.get('Content-Title');
+        const contentFavicon = response.headers.get('Content-Favicon');
+
+        // Set the title and favicon in your HTML
+        document.title = contentTitle;
+        const faviconLink = document.querySelector("link[rel*='icon']");
+        faviconLink.href = contentFavicon;
+
+        // Handle the rest of your proxy logic and rendering
     });
 });

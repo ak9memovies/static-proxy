c62661eb1eac3372b314b1e3b7cbf5a6e52e150c
ak9memovies <132725786+ak9memovies@users.noreply.github.com>
2023-11-06 15:13:28 -0600

    Update EncodedURL.js

--- public/js/EncodedURL.js
+++ public/js/EncodedURL.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const proxyForm = document.getElementById('proxyForm');
     const urlInput = document.getElementById('urlInput');
+    const proxyButton = document.getElementById('proxyButton');
 
-    proxyForm.addEventListener('submit', async function(event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-
+    proxyButton.addEventListener('click', function() {
+         // Get the user-provided URL
         const targetURL = urlInput.value;
-
-        // Fetch the proxied content, including the headers
-        const response = await fetch(`/mathcalculator?url=${encodeURIComponent(targetURL)}`);
-
-        // Handle the rest of your proxy logic and rendering
+                
+        // Redirect to the proxied URL without appending query parameters
+        window.location.href = '/mathcalculator?url=' + encodeURIComponent(targetURL);
     });
 });

9a1a9c0061e9f669e93ad9b25b18a594d8147df1
ak9memovies <132725786+ak9memovies@users.noreply.github.com>
2023-11-06 14:41:07 -0600

    Create ThemeSettings.js

---
+++ public/css/js/ThemeSettings.js
@@ -0,0 +1,14 @@
+:root {
+    --text-color: #333;
+    --background-color: #fff;
+}
+
+[data-theme="dark"] {
+    --text-color: #fff;
+    --background-color: #333;
+}
+
+body {
+    color: var(--text-color);
+    background-color: var(--background-color);
+}
